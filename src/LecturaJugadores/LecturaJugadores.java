504ad673e13e6676b8353a95ae8cdf7e0b27052b
PEDRO <pdguisem@alu.edu.gva.es>
2025-03-31 13:41:53 +0200

    Ficheros

--- src/LecturaJugadores/LecturaJugadores.java
+++ src/LecturaJugadores/LecturaJugadores.java
@@ -1,8 +1,6 @@
 package LecturaJugadores;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,12 +8,27 @@ public class LecturaJugadores {
     public static void main(String[] args) {
         List<Jugadores>jugadores=new ArrayList<>();
         try{
-            FileReader fileReader=new FileReader("/home/usuario/Descargas/jugadores.txt");
+            FileReader fileReader=new FileReader("C:\\Users\\PEDRO\\Downloads\\jugadores.txt");
             BufferedReader bufferedReader=new BufferedReader(fileReader);
+            FileWriter fileWriter=new FileWriter("C:\\Users\\PEDRO\\Downloads\\alojados.txt");
+            BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
             String cadena= bufferedReader.readLine();
-            while (cadena.equalsIgnoreCase("H")){
+            while (cadena!=null){
+                String[]cadenas=cadena.split(";");
+                if(cadenas.length>=7){
+                    String info=cadenas[7];
+                    if(info.contains("H")){
+                        String ran=cadenas[0];
+                        String nombrejugador=cadenas[2];
+                        bufferedWriter.write(ran + ";" + nombrejugador);
+                        bufferedWriter.newLine();
+                    }
+
+                }
                 cadena= bufferedReader.readLine();
             }
+            bufferedWriter.close();
+            bufferedReader.close();
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
